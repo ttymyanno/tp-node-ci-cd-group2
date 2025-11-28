9f2ee89a6fd73bcff49d509c7ddbe37e910db8dc
ttymyanno <yann.touassom@gmail.com>
2025-11-28 15:52:11 +0100

    Fix doublon import addition dans les tests

--- index.test.js
+++ index.test.js
@@ -3,7 +3,6 @@ const {multiplication,addition} = require ( './index' ) ;
 test ('multiplication de 2 * 3 = 6 ', () => {
 expect ( multiplication (2 , 3) ). toBe (6) ;
 }) ;
-const addition = require('./index');
 
 test('1 + 2 = 3', () => {
   expect(addition(1, 2)).toBe(3);
