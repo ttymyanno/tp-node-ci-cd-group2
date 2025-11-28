5a0ccf3747f11b27a80a04dfc7591cd9303ff301
Oumayma CHOUICHI <oumaymasaoudi6@gmail.com>
2025-11-28 14:10:30 +0100

    Travail individuel + modification YAML

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function addition (a , b) {
-return a + b ;
+function addition(a, b) { 
+  return a + b; 
 }
-module . exports = addition ;
+module.exports = addition;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const addition = require ( './index' ) ;
+const addition = require('./index');
 
-test ('addition de 1 + 2 = 3 ', () => {
-expect ( addition (1 , 2) ). toBe (3) ;
-}) ;
+test('1 + 2 = 3', () => {
+  expect(addition(1, 2)).toBe(3);
+});
